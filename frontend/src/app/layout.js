25dd0c513f63319d2c7005674c4fd25614af81bc
fanaardie25 <follgrowthindonesia@gmail.com>
2025-11-12 10:35:30 +0700

    feat: add scrool smooth

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
         <head>
         {/* CSS Summernote */}
         <link
